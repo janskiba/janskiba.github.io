3e6c5347b6e4f483dedb1da848e7ecc7cb6e27a9
janskiba <jan.skiba.poczta@proton.me>
2026-07-18 21:13:24 +0200

    feat: change outup directory do docs for github pages compatibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://janskiba.dev',
+  outDir: 'docs',
   vite: {
     plugins: [tailwindcss()]
   },
